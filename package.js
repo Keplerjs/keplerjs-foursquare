8781478a7fb0b2287111c619260ccc657c90b71d
stefanocudini <stefano.cudini@gmail.com>
2018-10-08 00:00:31 +0200

    version 1.3.7

--- package.js
+++ package.js
@@ -1,8 +1,7 @@
-
 Package.describe({
+  version: "1.3.7",
   name: "keplerjs:foursquare",
   summary: "Keplerjs plugin for Foursquare API",
-  version: "1.3.0",
   git: "https://github.com/Keplerjs/keplerjs-foursquare.git"
 });
 
@@ -17,7 +16,7 @@ Package.onUse(function (api) {
   api.versionsFrom("1.5.1");
 
   api.use([
-    'keplerjs:core@1.3.0',
+    'keplerjs:core@1.3.7',
   ]);
 
   api.addFiles([
